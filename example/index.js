a4d7ab9055f4c8573fe6cd66c0b18c46cf6e1f19
Clément Pasquier <clement@n00sphere.net>
2017-06-12 19:58:12 +0200

    first commit

---
+++ example/index.js
@@ -0,0 +1,30 @@
+import 'zwip/src/polyfills';
+import 'pwet/src/polyfills';
+import Component from 'pwet/src/component';
+import ZwipPlayer from 'zwip-player';
+import { renderDiv } from 'idom-util';
+import BubbleAnimation from '../src/animation';
+
+Component.define(ZwipPlayer);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const zwipPlayer = document.createElement('zwip-player');
+
+  zwipPlayer.update({
+    makeAnimation(scene) {
+
+      return BubbleAnimation({
+        container: scene,
+        element: scene.firstChild,
+        duration: 4000
+      });
+    },
+    renderScene() {
+
+      renderDiv(null, null, 'class', 'circle', null);
+    },
+  });
+
+  document.body.insertBefore(zwipPlayer, document.body.firstChild);
+});
---
+++ example/webpack.config.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const Path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+
+module.exports = (env) => {
+
+  const isBuild = env === 'production' || env === 'prod';
+
+  const port = 3000;
+
+  const entry = Path.resolve(__dirname, './index.js');
+
+  const output = {
+    path: Path.resolve(__dirname, './build'),
+    filename: '[name].js'
+  };
+
+  const template = Path.resolve(__dirname, 'index.html');
+
+  const devtool = !isBuild && 'source-map';
+
+  const devServer = {
+    port,
+    historyApiFallback: true,
+    inline: !isBuild,
+    hot: !isBuild,
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: true,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      }
+    }
+  };
+
+  const loaders = [
+    {test: /\.js$/, use: 'babel-loader' }
+  ];
+
+  const plugins = [new Webpack.NamedModulesPlugin()];
+
+  if (!isBuild)
+    plugins.push(new Webpack.HotModuleReplacementPlugin());
+
+  plugins.push(new HTMLWebpackPlugin({
+    filename: `index.html`,
+    template,
+    cache: true,
+    inject: 'body',
+    minify: isBuild && {
+      collapseWhitespace: true,
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    },
+  }));
+
+  return {
+    entry,
+    output,
+    devtool,
+    devServer,
+    module: {
+      loaders
+    },
+    plugins
+  };
+
+};
